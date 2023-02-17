b613fe0804e2dab42eddb361b12d9d708c619936
TianqingTang <90829091+TianqingTang@users.noreply.github.com>
2023-02-17 14:15:30 +1100

    underlined

--- src/landing-page.tsx
+++ src/landing-page.tsx
@@ -30,7 +30,7 @@ function LandingPage() {
 
             <div className="main-wrapper">
                 <Row justify={"start"} align={"middle"}>
-                    <Col style={{padding:'0', margin: '0'}} span={10}>
+                    <Col style={{padding: '0', margin: '0'}} span={10}>
                         <Row justify={"start"} align={"bottom"}>
                             <img className="main-nft-pic" width={'100%'} src={nft1} alt="nft"/>
                         </Row>
@@ -38,7 +38,11 @@ function LandingPage() {
                     <Col style={{margin: '0 0 0 200px'}}>
                         <SocialIcon className="main-logo" url="https://twitter.com/_classnow_" fgColor="#FDDD4E"
                                     bgColor="#363A59"/>
-                        <p className="main-sentence">COLLECT THE<br/>FIRST LAUNCH!</p>
+                        <p className="main-sentence">COLLECT THE&nbsp;</p>
+                        <Row>
+                            <p className="main-sentence-underlined">FIRST LAUNCH</p>
+                            <p className="main-sentence">!</p>
+                        </Row>
                         <p className="main-count">5000 / 5000 NFTs</p>
                         <a href="https://veace.xyz/">
                             <Button className="main-buy-button">BUY NOW</Button>
