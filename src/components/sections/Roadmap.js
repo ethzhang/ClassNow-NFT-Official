e72e06c878019e5a669f51aeca1d3f1b847898fd
TianqingTang <90829091+TianqingTang@users.noreply.github.com>
2023-03-01 16:04:41 +1100

    roadmap layout better

--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -17,60 +17,56 @@ const Roadmap = () => {
                      xl={{span: 20}}
                      xxl={{span: 20}}>
                     <Card className="roadmap-card">
-                        <p className="session-title">ROADMAP</p>
-                        <Row justify={"start"} align={"middle"}>
-                            <Col span={10}>
-                                <p className="roadmap-content-left">
+                        <p className="session-title" style={{padding:'0 64px'}}>ROADMAP</p>
+                        <Row gutter={[16, 32]} align={"middle"}>
+                            <Col span={12}>
+                                <p className="roadmap-content-right">
                                     <strong>Q2 2020</strong>
                                     <br/>Establishment of ClassNow
                                     <br/><strong>Q4 2022</strong>
                                     <br/>Turnover reaches 500,000 AUD</p>
                             </Col>
-                            <Col span={4}>
+                            <Col span={3}>
                                 <img className="roadmap-image" src={roadmap1} alt="nft"/>
                             </Col>
-                        </Row>
-                        <Row justify={"start"} align={"middle"}>
-                            <Col span={4} offset={10}>
+
+                            <Col span={3} offset={9}>
                                 <img className="roadmap-image" src={roadmap2} alt="nft"/>
                             </Col>
-                            <Col span={10}>
+                            <Col span={12}>
                                 <p className="roadmap-content-left">
                                     <strong>Q2 2023</strong>
                                     <br/>ClassNow Eager Emu NFT public sale</p>
                             </Col>
-                        </Row>
-                        <Row justify={"start"} align={"middle"}>
-                            <Col span={10}>
-                                <p className="roadmap-content-left">
+
+                            <Col span={12}>
+                                <p className="roadmap-content-right">
                                     <strong>Q3 2023</strong>
                                     <br/>ClassNow token (CLN) sale
                                     <br/>ClassNow Learn/Share & Earn program</p>
                             </Col>
-                            <Col span={4}>
+                            <Col span={3}>
                                 <img className="roadmap-image" src={roadmap3} alt="nft"/>
                             </Col>
-                        </Row>
-                        <Row justify={"start"} align={"middle"}>
-                            <Col span={4} offset={10}>
+
+                            <Col span={3} offset={9}>
                                 <img className="roadmap-image" src={roadmap4} alt="nft"/>
                             </Col>
-                            <Col span={10}>
+                            <Col span={12}>
                                 <p className="roadmap-content-left">
                                     <strong>Q4 2023</strong>
-                                    <br/>ClassNow DAO (decentralized autonomous organization)
-                                    <br/>Community governance of ClassNow</p>
+                                    <br/>ClassNow DAO
+                                    <br/>(decentralized autonomous organization)
+                                    </p>
                             </Col>
-                        </Row>
-                        <Row justify={"start"} align={"middle"}>
-                            <Col span={10}>
-                                <p className="roadmap-content-left">
+
+                            <Col span={12}>
+                                <p className="roadmap-content-right">
                                     <strong>Q1 2024</strong>
                                     <br/>ClassNow ICO (initial coin offering)
-                                    <br/>Integration of blockchain technology into the ClassNow platform to enhance
-                                    security and transparency</p>
+                                    <br/>Integration of blockchain into ClassNow platform</p>
                             </Col>
-                            <Col span={4}>
+                            <Col span={3}>
                                 <img className="roadmap-image" src={roadmap5} alt="nft"/>
                             </Col>
                         </Row>
